a12f76892066362a7519d19ad159bf42cbcaab56
BochinskiM <mcbochinski@gmail.com>
2023-02-06 15:35:20 +0100

    Add min function

--- src/code_example.py
+++ src/code_example.py
@@ -8,3 +8,12 @@ def sum_image(matrix: np.ndarray) -> float:
     :return: the sum of the matrix
     """
     return np.sum(matrix)
+
+
+def min_image(matrix: np.ndarray) -> float:
+    """
+    Function that returns the min of a matrix
+    :param matrix: an array matrix
+    :return: the min of the matrix
+    """
+    return np.min(matrix)
