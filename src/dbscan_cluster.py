8fc7157b078adaf59080465c023a9344dc2cf778
BochinskiM <mcbochinski@gmail.com>
2023-02-23 12:42:24 +0100

    Adding clustering scripts

--- src/dbscan_cluster.py
+++ src/dbscan_cluster.py
@@ -1,17 +1,50 @@
 from sklearn.cluster import DBSCAN
 import numpy as np
+import random
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 
 from process_density import preprocess
 
 
-x, y = preprocess()
-angles = np.empty((11433, 2))
+def dbscan():
+	x, y = preprocess()
+	angles = np.empty((len(x), 2))
 
-for i in range(0, len(x)):
-    angles[i,0] = x[i]
-    angles[i,1] = y[i] 
+	for i in range(0, len(x)):
+		angles[i,0] = x[i]
+		angles[i,1] = y[i] 
+
+	model = DBSCAN(eps=5, min_samples=8)
+	cluster = model.fit(angles)
+	labels = cluster.fit_predict(angles)
+
+	plot_cluster(angles, labels+1)
 
 
-print(angles)
+def plot_cluster(data, label):
+	colors = mcolors.CSS4_COLORS
+	nb_cluster = len(np.unique(label))
 
+	for i in range(0, nb_cluster):
+		filter = f"label{i} = data[label == {i}]"
+		exec(filter)
+		if i == 0:
+			scatter = f"plt.scatter(label{i}[:,0], label{i}[:,1], 1, color = 'w')"
+			exec(scatter)
+		else:
+			c = random.choice(list(colors.keys()))
+			scatter = f"plt.scatter(label{i}[:,0], label{i}[:,1], 1, color = '{colors[c]}')"
+			exec(scatter)
+
+	plt.title("η-θ conformational space")
+	plt.xlabel("η (degrees)")
+	plt.ylabel("θ (degrees)")
+	plt.axis([0, 360, 0, 360])
+	plt.xticks(np.arange(0, 361, 36))
+	plt.yticks(np.arange(0, 361, 36))
+	print("DBscan clustering done")
+	plt.show()
+
+if __name__ == '__main__':
+	 dbscan()
---
+++ src/kmean.py
@@ -0,0 +1,67 @@
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import random
+
+
+def recup_angle():
+	eta, theta, list_angle = [], [], []
+
+	with open("data/result.txt", "r") as filin:
+		for line in filin:
+			values = line.split()
+			if values[0] != "THETA" and values[0] != "NA" and values[1] != "NA":
+				theta.append(float(values[0]))
+				eta.append(float(values[1]))
+
+	for i in range(0, len(eta)):
+		list_angle.append([eta[i], theta[i]])
+
+	cluster_kmeans(list_angle)
+
+	return
+
+		
+def cluster_kmeans(list_angle):
+	X = np.array(list_angle)
+	cluster_model = KMeans(n_clusters = nb_clusters, n_init = "auto")
+	kmeans = cluster_model.fit(X)
+	labels = kmeans.fit_predict(X)
+	print("Clustering done")
+	plot_cluster(X, labels)
+
+	return
+
+
+def plot_cluster(data, label):
+	colors = ["b", "g", "m", "k", "y", "c", "r"]
+
+	for i in range(0,nb_clusters):
+		filter = f"label{i} = data[label == {i}]"
+		exec(filter)
+		rand_c = random.choice(colors)
+		colors.remove(rand_c)
+		scatter = f"plt.scatter(label{i}[:,0], label{i}[:,1], 0.5, color = '{rand_c}')"
+		exec(scatter)
+
+	plt.title("η-θ conformational space")
+	plt.xlabel("η (degrees)")
+	plt.ylabel("θ (degrees)")
+	plt.axis([0, 360, 0, 360])
+	plt.xticks(np.arange(0, 361, 36))
+	plt.yticks(np.arange(0, 361, 36))
+	print("Kmean clustering done")
+	plt.show()
+
+	return
+
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('n', type = int, choices = range(2, 8), help = "Specify the number of clusters")
+	args = parser.parse_args()
+	nb_clusters = args.n
+
+	recup_angle()
