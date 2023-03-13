d48bb89314c6b4c96d9d01d8bb8f7ccdc30e2ef6
clementbernardd <clement.bernard@univ-evry.fr>
2023-03-13 16:15:19 +0100

    add(visio code)

---
+++ src/pipeline.py
@@ -0,0 +1,74 @@
+import argparse
+import os
+from typing import Optional
+
+from src.clustering_method import dbscan_cluster
+
+
+class Pipeline:
+    def __init__(self,
+                training_path: str,
+                testing_path: str,
+                nb_cluster: int = 2,
+                model_name: Optional[str] = None,
+                 ):
+        """
+        Initialise the different parameters
+        """
+        self.training_path = training_path
+        self.testing_path = testing_path
+        self.nb_cluster = nb_cluster
+        self.model_name = model_name
+
+
+
+    def train(self):
+        print(f'{self.training_path}')
+        return None
+
+    def test(self):
+        print(f"{self.testing_path}")
+        return None
+
+    def fit_model(self, model_name: str):
+        if model_name == "dbscan":
+            dbscan_cluster()
+
+    def preprocess(self, input_path: str):
+        """
+        Call the C++
+        """
+        if input_path.endswith(".csv"):
+            # EASY
+            pass
+        else:
+            os.system(f"bin/c_file --input_file {input_path}")
+
+
+    def main(self):
+        self.train()
+        self.fit_model(self.model_name)
+        self.test()
+
+    @staticmethod
+    def get_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            "--training_path",
+            dest="training_path",
+            type=str,
+            help="",
+        )
+        parser.add_argument(
+            "--testing_path",
+            dest="testing_path",
+            type=str,
+             help="",
+        )
+        args = parser.parse_args()
+        return args
+
+if __name__ == "__main__":
+    args = Pipeline.get_arguments()
+    score_cli = Pipeline(**vars(args))
+    score_cli.main()
---
+++ tests/unit/test_model.py
@@ -0,0 +1,41 @@
+"""
+Unit test to check if a model is well saved and can be loaded
+"""
+import os
+import unittest
+
+
+from src.utils import save_model, load_model
+
+path = os.path.join("tests", "data", "test_unit.txt")
+
+
+class ModelTest(unittest.TestCase):
+
+    def test_save_model(self):
+        """
+        Test the save model function
+        """
+        save_model(None, path)
+        self.assertTrue(os.path.isfile(path))
+        os.remove(path)
+
+    def test_load_model(self):
+        """
+        Test if the model is being load properly
+        """
+        save_model(None, path)
+        model_load = load_model(path)
+        self.assertEqual(['test'], model_load)
+        os.remove(path)
+        # TODO : INFERENCE APRES FIT
+        # model_load = load_model(path)
+        # x_test, y_test = [0, 1, 2] , [0, 0, 1]
+        # y_pred = model_load.predict(x_test)
+        # self.assertEqual(y_pred, y_test)
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
