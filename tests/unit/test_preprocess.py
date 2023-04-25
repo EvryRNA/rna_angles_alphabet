d6e6ccd340e366c7482aab817e79bca99eb1d3ba
BochinskiM <mcbochinski@gmail.com>
2023-04-25 12:18:57 +0200

    Unit test for preprocessing

--- tests/unit/test_preprocess.py
+++ tests/unit/test_preprocess.py
@@ -1,17 +1,14 @@
 """
-Unit test to check the preprocessing
+Unit tests to check the preprocessing
 """
 import os
 import unittest
 
-import numpy as np
 from src.utils import get_angle
 from src.preprocessing.protein_prep import ProteinPrep
 from src.preprocessing.rna_prep import RNAPrep
 
 path = os.path.join("tests", "data")
-path_rna = os.path.join("tests", "data", "test_rna.pdb")
-path_prot = os.path.join("tests", "data", "test_prot.pdb")
 
 class ModelTest(unittest.TestCase):
 
@@ -20,7 +17,9 @@ class ModelTest(unittest.TestCase):
         Test the rna preprocessing
         """
         class_prep = RNAPrep()
-        class_prep.get_values(path_rna, "test_rna", path)
+        class_prep.get_values(f"{path}/test_rna.pdb", "test_rna", path)
+        test_array = get_angle(f"{path}/test_rna_values.csv", "rna")
+        self.assertEqual([155.462,247.584], [test_array[0][0], test_array[0][1]])
 
 
     def test_get_prot_values(self):
@@ -28,22 +27,9 @@ class ModelTest(unittest.TestCase):
         Test the protein preprocessing
         """
         class_prep = ProteinPrep()
-        class_prep.get_values(path_prot, "test_prot", path)
+        class_prep.get_values(f"{path}/test_prot.pdb", "test_prot", path)
         test_array = get_angle(f"{path}/test_prot_values.csv", "protein")
         self.assertEqual([131.987, 85.955], [test_array[0][0], test_array[0][1]])
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
